42e0c87a74740dd6e82b0b2effe287579d197e9d
Антон Кузянин <marswaitsfor@gmail.com>
2025-12-15 01:12:40 +0300

    created file1, file2 and Differ.java, modified bgk and App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,17 +1,29 @@
 package hexlet.code;
 
+import java.io.File;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
-
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 import java.util.concurrent.Callable;
+
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
         description = "Compares two configuration files and shows a difference")
 class App implements Callable<Integer> {
 
+    @Parameters(index = "0", paramLabel = "filepath1", description = "path to first file")
+    private File filepath1;
+    @Parameters(index = "1", paramLabel = "filepath2", description = "path to second file")
+    private File filepath2;
+
+    @Option(names = {"-f", "--format"}, paramLabel = "format", description = "output format [default: stylish]")
+    private String format;
+
     @Override
     public Integer call() throws Exception {
-        System.out.println("Please provide two files to compare");
+        var diff = Differ.generate(filepath1.getPath(), filepath2.getPath());
+        System.out.println(diff);
         return 0;
     }
 
---
+++ app/src/main/java/hexlet/code/Differ.java
@@ -0,0 +1,91 @@
+package hexlet.code;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeSet;
+import java.util.Objects;
+
+public class Differ {
+    public static String generate(String filePath1, String filePath2) throws Exception {
+        String readContent1 = Files.readString(Paths.get(filePath1));
+        String readContent2 = Files.readString(Paths.get(filePath2));
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String, Object> map1 = objectMapper.readValue(readContent1, Map.class);
+        Map<String, Object> map2 = objectMapper.readValue(readContent2, Map.class);
+
+        TreeSet<String> allKeys = new TreeSet<>(map1.keySet());
+        allKeys.addAll(map2.keySet());
+        List<Map<String, Object>> result = new ArrayList<>();
+        for (var key : allKeys) {
+            boolean inMap1 = map1.containsKey(key);
+            boolean inMap2 = map2.containsKey(key);
+            var value1 = map1.get(key);
+            var value2 = map2.get(key);
+            if (inMap1 && !inMap2) {
+                Map<String, Object> diff1 = new HashMap<>();
+                diff1.put("key", key);
+                diff1.put("status", "removed");
+                diff1.put("value", value1);
+                result.add(diff1);
+            } else if (!inMap1 && inMap2) {
+                Map<String, Object> diff2 = new HashMap<>();
+                diff2.put("key", key);
+                diff2.put("status", "added");
+                diff2.put("value", value2);
+                result.add(diff2);
+            } else if (Objects.equals(value1, value2)) {
+                Map<String, Object> diff3 = new HashMap<>();
+                diff3.put("key", key);
+                diff3.put("status", "unchanged");
+                diff3.put("value", value2);
+                result.add(diff3);
+            } else {
+                Map<String, Object> diff4 = new HashMap<>();
+                diff4.put("key", key);
+                diff4.put("status", "changed");
+                diff4.put("oldValue", value1);
+                diff4.put("newValue", value2);
+                result.add(diff4);
+            }
+        }
+        return format(result);
+    }
+    public static String format(List<Map<String, Object>> result) {
+
+        StringBuilder output = new StringBuilder("{\n");
+
+        for (var diff : result) {
+            String key = (String) diff.get("key");
+            String status = (String) diff.get("status");
+            var value = diff.get("value");
+
+            switch (status) {
+                case "removed":
+                    output.append("  - " + key + ": " + value + "\n");
+                    break;
+                case "added":
+                    output.append("  + " + key + ": " + value + "\n");
+                    break;
+                case "unchanged":
+                    output.append("    " + key + ": " + value + "\n");
+                    break;
+                case "changed":
+                    var oldValue = diff.get("oldValue");
+                    var newValue = diff.get("newValue");
+                    output.append("  - " + key + ": " + oldValue + "\n");
+                    output.append("  + " + key + ": " + newValue + "\n");
+                    break;
+                default:
+                    throw new RuntimeException("Unknown status: " + status);
+            }
+        }
+        output.append("}");
+        return output.toString();
+    }
+}
