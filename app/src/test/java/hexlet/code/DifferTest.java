9fcaab400745e3961a97447b3ee432641ca8c26a
Антон Кузянин <marswaitsfor@gmail.com>
2025-12-22 01:14:50 +0300

    created tests and  fixtures

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -1,12 +1,54 @@
 package hexlet.code;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 
 public class DifferTest {
+
+    private static Path getFixturePath(String fileName) {
+        return Paths.get("src", "test", "resources", "fixtures", fileName)
+                .toAbsolutePath()
+                .normalize();
+    }
+
     @Test
-    public void testDifferCreation() {
-        Differ differ = new Differ();
-        assertNotNull(differ, "Объект Differ должен создаваться");
+    public void testIdenticalValues() throws Exception {
+        Path path1 = getFixturePath("identical1.json");
+        Path path2 = getFixturePath("identical2.json");
+
+        String result = Differ.generate(path1.toString(), path2.toString());
+
+        assertTrue(result.contains("    host: hexlet.io"));
+        assertTrue(result.contains("    timeout: 50"));
+        assertTrue(result.contains("    proxy: 123.234.53.22"));
+        assertTrue(result.contains("    follow: false"));
+
+        assertFalse(result.contains("  - "));
+        assertFalse(result.contains("  + "));
+
+        assertTrue(result.startsWith("{"));
+        assertTrue(result.endsWith("}"));
+    }
+
+    @Test
+    public void testDifferentValues() throws Exception {
+        Path path1 = getFixturePath("diff1.json");
+        Path path2 = getFixturePath("diff2.json");
+
+        String result = Differ.generate(path1.toString(), path2.toString());
+
+        assertTrue(result.contains("    host: hexlet.io"));
+        assertTrue(result.contains("  + timeout: 20"));
+        assertTrue(result.contains("  - proxy: 123.234.53.22"));
+        assertTrue(result.contains("  + follow: true"));
+        assertTrue(result.contains("  + verbose: true"));
+
+        assertTrue(result.startsWith("{"));
+        assertTrue(result.endsWith("}"));
     }
 }
